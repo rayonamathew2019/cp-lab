38918d2923d70b65ae5e6cd05037054d4e2f5f75
rayonamathew2019 <47378995+rayonamathew2019@users.noreply.github.com>
2019-02-27 12:23:02 +0530

    Add files via upload

---
+++ amstrong.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int num,ams=0,rem,amstrong;
+printf("enter the number");
+scanf("%d",&num);
+amstrong=num;
+while(num>0)
+{
+	rem=num%10;
+	ams=ams+rem*rem*rem;
+	num=num/10;
+	
+}
+if(amstrong==ams)
+	printf("%d is amstrong",ams);
+else
+	printf("%d is not amstrong",ams);
+}
