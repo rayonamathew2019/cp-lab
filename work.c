9dd7b4c36e93b92ef028da4d419e7310b229906f
devinair14 <44498633+devinair14@users.noreply.github.com>
2021-10-31 21:22:29 +0530

    Update work.c

--- work.c
+++ work.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 void main()
 {
-	int hour;
+	int hours;
 	printf("enter the hour");
-	scanf("%d",&hour);
-	if((hour>=2)&&(hour<3))
+	scanf("%d",&hours);
+	if((hours>=2)&&(hours<3))
 		printf("worker is highly efficent");
-	if((hour>=3)&&(hour<4))
+	if((hours>=3)&&(hours<4))
 		printf("worker is to improve speed");
-	if((hour>=4)&&(hour<5))
+	if((hours>=4)&&(hours<5))
 		printf("worker is given training to improve speed");
-	if(hour>=5)
+	if(hours>=5)
 		printf("the worker is terminated");
 }
